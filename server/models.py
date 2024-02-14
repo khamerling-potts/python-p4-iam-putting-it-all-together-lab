aeeb36b320f5799b0e7dd115ef73556931d939a9
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-02-14 16:30:32 -0500

    fixing password decoding

--- server/models.py
+++ server/models.py
@@ -27,7 +27,7 @@ class User(db.Model, SerializerMixin):
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
-        self._password_hash = password_hash
+        self._password_hash = password_hash.decode("utf-8")
 
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
